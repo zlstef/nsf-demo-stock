f8f1c928458911e80ed675866930178f63b6ac4a
zlstef <z.liang@msn.com>
2022-03-10 15:05:12 +0800

    Update run.sh

--- nsf-demo-stock-viewer/run.sh
+++ nsf-demo-stock-viewer/run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-java -javaagent:/opt/nsf/nsf-agent.jar=nsf -javaagent:/opt/apm/napm-java-agent/napm-java-rewriter.jar=conf=napm-agent.properties -jar /opt/nsf/nsf-demo-stock-viewer-1.0-SNAPSHOT.jar
+#java -javaagent:/opt/nsf/nsf-agent.jar=nsf -javaagent:/opt/apm/napm-java-agent/napm-java-rewriter.jar=conf=napm-agent.properties -jar /opt/nsf/nsf-demo-stock-viewer-1.0-SNAPSHOT.jar
+java -javaagent:/opt/nsf/nsf-agent.jar=nsf -javaagent:/opt/apm/skywalking-napm-bin-8.5.0-latest/agent/skywalking-agent.jar -Dskywalking_config=/opt/apm/skywalking-napm-bin-8.5.0-latest/agent/config/agent.config -jar /opt/nsf/nsf-demo-stock-viewer-1.0-SNAPSHOT.jar
